ccbbdd31f4a9986fed908a0b9efc37cbd4778442
Joonasmikael97 <124892239+Joonasmikael97@users.noreply.github.com>
2025-07-14 01:59:08 +0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -7,7 +7,7 @@ def home():
     return "Discord bot online"
 
 def run():
-    app.run(host="0.0.0.0, port=8080")
+    app.run(host='0.0.0.0', port=8080)
     
 def keep_alive():
     t = Thread(target=run)
