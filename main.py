b504e6311005621fa5707e187b1d8ea785d94eb0
Joonasmikael97 <124892239+Joonasmikael97@users.noreply.github.com>
2025-07-14 02:35:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ async def weather(ctx, *, city: str = None):
 
         embed = discord.Embed(
             title=f"Sää {location}, {country}",
-            description=f"📌 {condition}\n🌡️ {temp_c}°C (Tuntuu kuin {feels_like}°C)\n\n📢 *Pekka Pouta sanoisi:* _{quote}_",
+            description=f"📌 {condition}\n🌡️ {temp_c}°C (Tuntuu kuin {feels_like}°C)\n _{quote}_",
             color=0x1abc9c
         )
         embed.set_thumbnail(url=icon_url)
